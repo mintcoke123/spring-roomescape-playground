c32009e4fba18ce50b987710a2dcca6827581a54
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-11-06 19:29:01 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -6,9 +6,6 @@ public class Reservation {
     private String date;
     private String time;
 
-    public Reservation() {
-    }
-
     public Reservation(int id, String name, String date, String time) {
         this.id = id;
         this.name = name;
@@ -16,21 +13,7 @@ public class Reservation {
         this.time = time;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
 }
 
 
