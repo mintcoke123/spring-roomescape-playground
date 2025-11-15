b92cdfb777cd17586ebae57cf31fa8142e0e5823
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-11-15 23:52:50 +0900

    feat: 예외 핸들러 추가

--- src/main/java/roomescape/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/exception/ReservationExceptionHandler.java
@@ -1,17 +1,29 @@
 package roomescape.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ReservationExceptionHandler {
 
     @ExceptionHandler({
             InvalidReservationRequestException.class,
-            NotFoundReservationException.class
+            NotFoundReservationException.class,
+            IllegalArgumentException.class,
+            HttpMessageNotReadableException.class,              
+            MethodArgumentTypeMismatchException.class,          
+            MissingServletRequestParameterException.class      
     })
     public ResponseEntity<Void> handleBadRequest(RuntimeException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleUnexpected(Exception e) {
+        return ResponseEntity.status(500).build();
     }
 }
